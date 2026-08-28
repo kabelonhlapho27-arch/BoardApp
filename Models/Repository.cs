7d3dbe76e2ef988323bca341bdb9554c272161fc
kabelo nhlapho <kabelonhlapho27@icloud.com>
2026-08-28 11:41:15 +0200

    Update the repository list and add method

---
+++ Models/Repository.cs
@@ -0,0 +1,19 @@
+﻿namespace BoardApp.Models
+{
+    public class Repository
+    {
+        private static List<Board> boards = new List<Board>();
+
+        public static IEnumerable<Board> Boards
+        {
+            get { return boards; }
+        }
+
+        public static void AddBoard(Board board)
+        {
+            boards.Add(board);
+        }
+
+    }
+}
+
