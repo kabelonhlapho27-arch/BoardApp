80eefd3d5259730988ad660362d51c7401a31dce
kabelo nhlapho <kabelonhlapho27@icloud.com>
2026-08-28 18:50:55 +0200

    Adding new action properties on the new board controller

---
+++ Controllers/BoardController.cs
@@ -0,0 +1,20 @@
+﻿using BoardApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BoardApp.Controllers
+{
+    public class BoardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(Repository.Boards);
+        }
+
+        public IActionResult Details(string boardCode)
+        {
+            var board = Repository.Boards.FirstOrDefault(b => b.BoardCode == boardCode);
+            return View(board);
+        }
+    }
+}
