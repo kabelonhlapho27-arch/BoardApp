7d7787ed612a3a5fbbff0fd35df7afc6ebd3401e
Philasande Simelane <philasandesimelane133@gmail.com>
2026-09-01 18:57:29 +0200

    Update comments and formatting in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
+// Group leader name : Kabelo Nhlapho
+// Group Student nrs : 219005935; 224042163; 224037409; 220048471; 223068452; 224136508; 224069913; 219005935
+// Assignment nr     : SOD226C Practical Assessment 1 · 2026
+// Purpose           : The purpose of this program is to configure application services,
+//                     set up the HTTP request middleware pipeline, and start the web application.
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -11,7 +17,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-}
+} // end if
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
